ccb5c695adf3a087d8830a41d0bcb720d4662885
Lampros Grammatikopoulos <lambrosgrammat@gmail.com>
2021-08-14 12:11:24 +0300

    Add files via upload

---
+++ Exercise 2/Projects/P6/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int yylex();
+extern FILE* yyin;
+FILE* fout;
+
+int main(int argc, char* argv[])
+{
+	if (argc < 3)
+	{
+		printf("\nError!!! Missing Command line arguments");
+		printf("\nUsage exe <inputfile> <outputfile>");
+		exit(1);
+	}
+	else
+	{
+		fout = fopen(argv[2], "w");
+		if (fout == NULL)
+		{
+			printf("Error openning file.\n");
+		}
+		yyin = fopen(argv[1], "r");
+		if (yyin == NULL)
+		{
+			printf("Error openning file.\n");
+		}
+		yylex();
+	}
+	fclose(fout);
+	fclose(yyin);
+}
---
+++ Exercise 2/Projects/P7/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int yylex();
+extern FILE* yyin;
+FILE* fout;
+
+int main(int argc, char* argv[])
+{
+	if (argc < 3)
+	{
+		printf("\nError!!! Missing Command line arguments");
+		printf("\nUsage exe <inputfile> <outputfile>");
+		exit(1);
+	}
+	else
+	{
+		fout = fopen(argv[2], "w");
+		if (fout == NULL)
+		{
+			printf("Error openning file.\n");
+		}
+		yyin = fopen(argv[1], "r");
+		if (yyin == NULL)
+		{
+			printf("Error openning file.\n");
+		}
+		yylex();
+	}
+	fclose(fout);
+	fclose(yyin);
+}
+
+//([*+\-&|<>=]{1,2})|([\/^]?)|([!][=]?)|([>][=]?)|([<][=]?) { fout=fopen("output7.txt","a"); fprintf_s(fout,"String found: {%s}\n",yytext); fclose(fout); }
